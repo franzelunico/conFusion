118e3e879858cd93ad672b3b231feafbcaf2b31b
Franz Lopez <franzelunico@gmail.com>
2016-06-14 15:58:55 -0400

    assigment 2 complete (new directive= new scope) use parent

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,8 +1,112 @@
 'use strict';
+
 angular.module('confusionApp', [])
+
+        .controller('MenuController', ['$scope', function($scope) {
+            
+            $scope.tab = 1;
+            $scope.filtText = '';
+            $scope.showDetails = false;
+
+            $scope.dishes=[
+                         {
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains', 
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comment: ''
+                        },
+                        {
+                          name:'Zucchipakoda', 
+                          image: 'images/zucchipakoda.png',
+                          category: 'appetizer', 
+                          label:'',
+                          price:'1.99',
+                          description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
+                           comment: ''
+                        },
+                        {
+                          name:'Vadonut', 
+                          image: 'images/vadonut.png',
+                          category: 'appetizer', 
+                          label:'New',
+                          price:'1.99',
+                          description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
+                           comment: ''
+                        },
+                        {
+                          name:'ElaiCheese Cake', 
+                          image: 'images/elaicheesecake.png',
+                          category: 'dessert', 
+                          label:'',
+                          price:'2.99',
+                          description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
+                           comment: ''
+                        }
+                        ];
+                        
+            $scope.select = function(setTab) {
+                $scope.tab = setTab;
+                
+                if (setTab === 2) {
+                    $scope.filtText = "appetizer";
+                }
+                else if (setTab === 3) {
+                    $scope.filtText = "mains";
+                }
+                else if (setTab === 4) {
+                    $scope.filtText = "dessert";
+                }
+                else {
+                    $scope.filtText = "";
+                }
+            };
+
+            $scope.isSelected = function (checkTab) {
+                return ($scope.tab === checkTab);
+            };
+    
+            $scope.toggleDetails = function() {
+                $scope.showDetails = !$scope.showDetails;
+            };
+        }])
+
+        .controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel === "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
         .controller('DishDetailController', ['$scope', function($scope) {
 
-            $scope.dish={
+            var dish={
                           name:'Uthapizza',
                           image: 'images/uthapizza.png',
                           category: 'mains', 
@@ -44,46 +148,53 @@ angular.module('confusionApp', [])
                            ]
                     };
             
-            $scope.filtText = '';
+            $scope.dish = dish;
+			$scope.filtText = '';
+
             $scope.getOrder = function(){
-                    var list_order = ["author","date","rating","-author","-date","-rating"];
-                    var found = "";
-                    var a = list_order.indexOf($scope.filtText);
-                    if(a > -1){
-                        found= list_order[a];
-                    }else{
-                        found= "";
-                    }
-                    return found;
+                console.log("controller DishDetailController: " + $scope.filtText);
+                var list_order = ["author","date","rating","-author","-date","-rating"];
+                var found = "";
+                var a = list_order.indexOf($scope.filtText);
+                if(a > -1){
+                    found= list_order[a];
+                }else{
+                    found= "";
+                }
+                return found;
             };
-            $scope.ratingSelect=5;
-            $scope.newComment = {
-                rating:$scope.ratingSelect,
-                comment:"",
-                author:"",
-                date:""
-            };
+            
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
-            $scope.submitComment = function() {
-
-                console.log($scope.newComment);
-
-                if ( ($scope.newComment.comment !== "") && $scope.newComment.author !== "") {
-                    console.log('incorrect');
-                    $scope.newComment.date=new Date();
-                    $scope.dish.comments.push($scope.newComment);
-                    $scope.commentForm.$setPristine();
-
-                    $scope.newComment = {
-                        rating:$scope.ratingSelect,
-                        comment:"",
-                        author:"",
-                        date:""
-                    };
-                }
+            
+            //Step 1: Create a JavaScript object to hold the comment from the form
+			$scope.ratingSelect=5;
+			$scope.newComment = {
+				rating:$scope.ratingSelect,
+				comment:"",
+				author:"",
+				date:""
+			};
+            $scope.submitComment = function () {
+                
+                //Step 2: This is how you record the date
+                $scope.newComment.date = new Date().toISOString();
+                
+                // Step 3: Push your comment into the dish's comment array
+                $scope.dish.comments.push("Your JavaScript Object holding the comment");
+                
+                //Step 4: reset your form to pristine
+				$scope.commentForm.$setPristine();
+                
+                //Step 5: reset your JavaScript object that holds your comment
+				$scope.newComment = {
+					rating:$scope.ratingSelect,
+					comment:"",
+					author:"",
+					date:""
+				};
             };
-
         }])
+
 ;
